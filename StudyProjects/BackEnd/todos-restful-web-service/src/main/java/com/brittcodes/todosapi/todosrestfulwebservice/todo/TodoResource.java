136a9f49942c523c4c6d6c3b4c24eecb6274730a
Brittany Tinnin <tinfullstackdev@gmail.com>
2021-02-21 17:57:44 -0700

    update

--- StudyProjects/BackEnd/todos-restful-web-service/src/main/java/com/brittcodes/todosapi/todosrestfulwebservice/todo/TodoResource.java
+++ StudyProjects/BackEnd/todos-restful-web-service/src/main/java/com/brittcodes/todosapi/todosrestfulwebservice/todo/TodoResource.java
@@ -2,8 +2,11 @@ package com.brittcodes.todosapi.todosrestfulwebservice.todo;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins = "http://localhost:4200")
@@ -24,6 +27,18 @@ public class TodoResource {
 	/*
 	 * Delete a Todo of a User DELETE /users/{username}/todos/{todo_id}
 	 */
+	@DeleteMapping("/users/{username}/todos/{id}")
+	public ResponseEntity<Void> deleteTodo(@PathVariable("username") String username, @PathVariable("id") long id ){
+		
+		Todo todo = todoService.deleteById(id);
+		
+		if(todo != null) {
+			return ResponseEntity.noContent().build();
+		}
+		
+		return ResponseEntity.notFound().build();
+		
+	}
 
 	/*
 	 * Edit/Update a Todo PUT /users/{user_name}/todos/{todo_id}
