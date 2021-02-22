8e3abb18e5314c287ce6e00e94ddbb46d8e91936
Brittany Tinnin <tinfullstackdev@gmail.com>
2021-02-21 22:14:16 -0700

    update

--- StudyProjects/FrontEnd/todo-manage-front/src/components/TodoComponent.jsx
+++ StudyProjects/FrontEnd/todo-manage-front/src/components/TodoComponent.jsx
@@ -1,6 +1,14 @@
 import React, {Component} from 'react'
 
 export default class TodoComponent extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            id: 1,
+            description: 'Learn Forms',
+            targetDate: new Date()
+        }
+    }
 
     render(){
         return <div>Todo Component for this id{this.props.match.params.id} </div>
