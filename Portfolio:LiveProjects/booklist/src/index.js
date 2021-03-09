da4d55310799dd74753e809e5d7378b4c15f339d
Brittany Tinnin <tinfullstackdev@gmail.com>
2021-03-09 10:24:53 -0700

    added some notes

--- Portfolio:LiveProjects/booklist/src/index.js
+++ Portfolio:LiveProjects/booklist/src/index.js
@@ -7,9 +7,6 @@ class Book {
   }
 }
 
-// const title = document.querySelector('#title').value;
-// const author = document.querySelector('#author');
-// const isbn = document.querySelector('#isbn');
 const bookForm = document.querySelector('#add-book-form');
 const bookList = document.querySelector('#book-list');
 
@@ -20,5 +17,30 @@ bookForm.addEventListener('submit', (event) => {
 
   console.log('submit form');
   const title = document.querySelector('#title').value;
-  console.log(title);
+  const author = document.querySelector('#author');
+  const isbn = document.querySelector('#isbn');
 });
+
+// Book Class: Represents a Book
+
+// UI Class Handle UI Tasks
+// Vanish alert in 3 seconds
+
+// Store Class: Handles Storage
+
+// Event: Display Books
+
+// Event: Add a book
+// Get form values
+// Validate form values
+// show alert if values empty -- from UI class
+// Instantiate a book
+// Add Book to UI -- from UI class
+// Add book to store -- from Store class
+// Show success message -- from UI class
+// Clear fields -- from UI class
+
+// Event Remove a Book
+// Remove book from UI -- from UI class
+// Remove book from store -- from Store class
+// Show success message -- from UI class
